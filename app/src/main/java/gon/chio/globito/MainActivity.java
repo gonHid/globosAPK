6f567cdf868c22fdcf002a087d1bd4474cc094b1
gonHid <gon.hidalgo.ch@gmail.com>
2022-08-07 17:48:39 -0400

    version 2

--- app/src/main/java/gon/chio/globito/MainActivity.java
+++ app/src/main/java/gon/chio/globito/MainActivity.java
@@ -2,80 +2,125 @@ package gon.chio.globito;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
+
 
 import android.animation.ObjectAnimator;
-import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    List<ImageView> globos =new ArrayList<ImageView>();;
-    int count=0;
+
+    List<ImageView> globos =new ArrayList<>(5);
+    int contador = 0;
+
     ConstraintLayout contenedor;
 
-    ImageView globo;
-    boolean arriba=true;
+
+    //manejarán el loop para los globos
+    private final Handler handler = new Handler();
+    private final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            //primero posicionar globos
+            posicionar();
+            // repetir
+            handler.postDelayed(runnable, 9500);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         contenedor = findViewById(R.id.contenedor);
-       globo = findViewById(R.id.globo);
-        AnimationDrawable animD = (AnimationDrawable) globo.getBackground();
-        animD.setEnterFadeDuration(1000);
-        animD.setExitFadeDuration(1000);
-        animD.start();
 
-       /* Toast toast = Toast.makeText(this, "primero: "+globos.get(0).getX()+" " +
-                "\nsegundo: "+globos.get(1).getX()+" " +"\ntercero: "+globos.get(2).getX(), Toast.LENGTH_LONG);
-        toast.show();*/
+        crearGlobos();
 
-        /*for(int i = 0; i < 3;i++){
-            (new Handler()).postDelayed(this::mover, 6000);
-        }*/
+        //inicar el loop
+        handler.post(runnable);
 
     }
 
 
 
     public void mover(){
-
-        ObjectAnimator animation = ObjectAnimator.ofFloat(globo, "translationY", -1000f);
-        animation.setDuration(5000);
+        //Aplicar la animacion de movimiento vertical- cada globo tiene un tamaño distinto, por lo que les doy un limite personalizado
+        //solo lo justo para desaparecer de pantalla
+        float limiteSup = globos.get(contador).getLayoutParams().height;
+        ObjectAnimator animation = ObjectAnimator.ofFloat(globos.get(contador), "translationY", -limiteSup);
+        animation.setDuration(10000);
         animation.start();
-        count++;
+        globos.get(contador).setVisibility(View.VISIBLE);
+        contador++;
+        if(contador == 5){
+            contador = 0;
+        }
+       /* Toast toast = Toast.makeText(MainActivity.this,"contador: "+contador, Toast.LENGTH_LONG);
+        toast.show();*/
+
     }
 
-    public void crear(){
-        for(int i=0;i<3;i++){
-            ImageView globo = new ImageView(this);
-            globo.setId(i);
-            globos.add(globo);
+    public void posicionar(){
+        //preparo una posicion previa al movimiento, la cual es al azar
+
+        int max = 9,  min = 0,posX;
+        int range = max - min + 1;
+
+
+        posX = (int)(Math.random() * range) + min;
+        posX = posX * 100;
+
+          /*Toast toast = Toast.makeText(this,"posX: "+ posX + "\n contador: "+contador, Toast.LENGTH_LONG);
+        toast.show();*/
+        globos.get(contador).setY(1800);
+        globos.get(contador).setX(posX);
+
+         mover();
+
+
+    }
+
+    public void crearGlobos(){
+        //creacion inicial del array con los ImageView para cada globo
+
+        for(int i=0; i < 5;i++){
+            globos.add(new ImageView(MainActivity.this));
             globos.get(i).setImageResource(R.drawable.globoi);
-            LinearLayout.LayoutParams tamanio = new LinearLayout.LayoutParams(175, 200);
-            globos.get(i).setLayoutParams(tamanio);
-            globos.get(i).setX(((i-1)*200)+300);
-            globos.get(i).setY(2000);
-            globos.get(i).setBackground(ContextCompat.getDrawable(this, R.drawable.background_list));
+            globos.get(i).setBackgroundResource(R.drawable.background_list);
 
             contenedor.addView(globos.get(i));
-            AnimationDrawable animD = (AnimationDrawable) globos.get(i).getBackground();
-            animD.setEnterFadeDuration(1000);
-            animD.setExitFadeDuration(1000);
-            animD.start();
-            (new Handler()).postDelayed(this::mover, 6000);
+
+            animColores(globos.get(i));
+
+            globos.get(i).setVisibility(View.INVISIBLE);
+            if(i == 0 || i==4){
+                globos.get(i).getLayoutParams().height = 200;
+                globos.get(i).getLayoutParams().width = 175;
+            }else if(i==1 || i==3){
+                globos.get(i).getLayoutParams().height = 280;
+                globos.get(i).getLayoutParams().width = 245;
+            }else {
+                globos.get(i).getLayoutParams().height = 360;
+                globos.get(i).getLayoutParams().width = 315;
+            }
+
+
         }
 
+    }
+    public void animColores(ImageView imagen) {
+        //Animacion simple del fondo para cada globo (es igual para todos)
+
+        AnimationDrawable animD = (AnimationDrawable) imagen.getBackground();
+        animD.setEnterFadeDuration(1000);
+        animD.setExitFadeDuration(1000);
+        animD.start();
     }
 
 }
